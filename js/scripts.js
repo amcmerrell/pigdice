d20e08aab3cf70ae5d4745c295f43bce635fd732
Andrew Merrell and Yusuf Qedan <andrew@>
2016-08-24 10:37:19 -0700

    add function to change turns.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 // Business Logic
+var numberOfPlayers = 0;
+var playerArray = [];
+var playerWins = false;
+
 function Player(name, turnTotal, scoreTotal) {
-  this.name = name;
+  this.nameOfPlayer = name;
   this.turnTotal = turnTotal;
   this.scoreTotal = scoreTotal;
 }
 
 Player.prototype.roll = function(){
   var roll = Math.floor((Math.random() * 6) + 1);
-  alert("you rolled" + roll);
+  alert(this.nameOfPlayer + " you rolled a " + roll);
   if (roll !== 1) {
     this.turnTotal += roll;
     return false;
@@ -25,16 +29,33 @@ Player.prototype.turn = function(){
     if ( !(rolledOne) && this.turnTotal < 20) {
       stop = confirm("Current turn total is:" + this.turnTotal + ". Would you like to roll again?");
     }else if(rolledOne) {
-      alert("You rolled a 1 too bad!")
+      console.log("You rolled a 1 too bad!")
     }
-  }while((this.turnTotal < 20 && stop) && !(rolledOne))
+  }while((this.turnTotal < 20 && stop) && !(rolledOne));
   this.scoreTotal = this.turnTotal;
+  if (this.scoreTotal >= 20) {
+    playerWins = true;
+  }
 }
+var play = function(){
+  do{
+    var myPlayer = new Player("Player " + (numberOfPlayers+1),0,0);
+    numberOfPlayers ++;
+    playerArray.push(myPlayer);
+  }while(numberOfPlayers < 2);
 
-var myPlayer = new Player("Player 1",0,0);
-myPlayer.turn();
-console.log("You scored: " + myPlayer.scoreTotal);
+  do{
+    for (var i = 0; i < playerArray.length; i++) {
+      playerArray[i].turn();
+      alert(playerArray[i].nameOfPlayer+ ": you scored: " + playerArray[i].scoreTotal);
+      if (playerWins){
+        console.log("Congratualtions Player " + playerArray[i].nameOfPlayer + " you win!!!");
+        break;
+      }
+    }
+  }while(!(playerWins));
+}
 // UI Logic
 $(document).ready(function() {
-
+ play();
 });
