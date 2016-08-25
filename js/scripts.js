1138abc9b0e4353067b2be50dba982b4ba895d88
Andrew Merrell and Yusuf Qedan <andrew@>
2016-08-25 15:27:50 -0700

    add ai for easy mode

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ function Game(){
   this.players = [];
   this.winningNumber = 0;
   this.lastroll = true;
+  this.gameType = false;
 }
 
 function Player(name,score) {
@@ -45,7 +46,34 @@ Game.prototype.changeTurn = function(){
   else{
     this.turn = 0;
   }
+  if(this.gameType && this.turn === 1){
+    this.computerTurn();
+  }
 }
+
+Game.prototype.computerTurn = function(){
+  setTimeout(function() {
+    if(roll()){
+      setTimeout(function() {
+        if(roll()){
+          setTimeout(function() {
+            hold();
+          }, 1000);
+        }
+      }, 1000);
+    }
+  }, 1000);
+}
+
+//  var passing = true;
+//   setInterval (function() {
+//     if(passing){
+//       passing = roll();
+//     }else {
+//       return;
+//     }
+//   }, 1000);
+// }
 
 Game.prototype.hold = function() {
   this.players[this.turn].scoreTotal += this.players[this.turn].turnTotal;
@@ -69,12 +97,50 @@ Game.prototype.resetGame = function(){
     this.players.pop();
   }
 }
-
 // UI Logic
-Game.prototype.updateScore = function(){
-  $(".score").remove();
+
+//callback function for roll click listener
+function roll(){
+  $("#message").text("");
+  myGame.players[myGame.turn].roll();
+  $("#roll-details").text(myGame.players[myGame.turn].nameOfPlayer);
+  myGame.players[myGame.turn].displayDie();
+  myGame.players[myGame.turn].evaluateRoll();
+  myGame.lastroll = myGame.players[myGame.turn].rollSuccess;
+  $("#turn-details").text(myGame.players[myGame.turn].nameOfPlayer + ": Your turn total is: " + myGame.players[myGame.turn].turnTotal);
+  if(myGame.players[myGame.turn].rollSuccess === false) {
+    myGame.changeTurn();
+    $("#message").text(myGame.players[myGame.turn].nameOfPlayer + "s Turn");
+  }else if(myGame.win()){
+    $("#message").text(myGame.players[myGame.turn].nameOfPlayer +" rolled a " + myGame.players[myGame.turn].currentRoll + " and Wins!!!");
+    $("#start").fadeIn();
+    $("#in-progress").hide();
+    $("#turn-details").text("");
+    $("#roll-details").text("");
+    myGame.resetGame();
+  }
+  return myGame.players[myGame.turn].rollSuccess;
+}
+
+//callback function for hold click listener
+function hold(){
+  if(myGame.lastroll){
+    myGame.hold();
+    $("#message").text(myGame.players[myGame.turn].nameOfPlayer + "s Turn");
+  }
+  else $("#message").text("You cant hold! You rolled a one!");
+}
+
+Game.prototype.createScoreBoard = function(){
+    $(".score").remove();
   for (i=0; i<this.players.length; i++) {
-    $("#scoreboard").append("<div class = 'score'><strong>" + this.players[i].nameOfPlayer + "</strong> Score: " +  this.players[i].scoreTotal + "</div>").hide().fadeIn();
+    $("#scoreboard").append("<div class = 'score'><strong>" + this.players[i].nameOfPlayer + "</strong> Score: " + "<span id = 'score" + i + "'>" + this.players[i].scoreTotal + "</span></div>").hide().fadeIn();
+  }
+}
+
+Game.prototype.updateScore = function(){
+  for (i=0; i<this.players.length; i++) {
+    $("#score"+i).text(this.players[i].scoreTotal);
   }
 }
 
@@ -82,48 +148,31 @@ Player.prototype.displayDie = function(){
   $(".die").hide();
   $("#number" + this.currentRoll).slideDown(50);
 }
-
+var myGame = new Game();
 $(document).ready(function() {
-  var myGame = new Game();
   $("form").submit(function(event) {
     event.preventDefault();
     var numberOfPlayers = parseInt($("#players").val());
     var winningNumber = parseInt($("#winning-number").val());
+    var gametype = $("input:radio[name=GameType]:checked").val();
     myGame.winningNumber = winningNumber;
+
+    if (gametype === "computer") {
+      numberOfPlayers = 2;
+      myGame.gameType = true;
+    }
+
     for (var i = 0; i < numberOfPlayers; i++) {
       myGame.createPlayers();
     }
+
     $("#message").text("");
     $("#start").hide();
     $("#in-progress").fadeIn();
     $(".die").hide();
-    myGame.updateScore();
+    myGame.createScoreBoard();
+
   });
-  $("#roll").click(function(){
-    $("#message").text("");
-    myGame.players[myGame.turn].roll();
-    $("#roll-details").text(myGame.players[myGame.turn].nameOfPlayer);
-    myGame.players[myGame.turn].displayDie();
-    myGame.players[myGame.turn].evaluateRoll();
-    myGame.lastroll = myGame.players[myGame.turn].rollSuccess;
-    $("#turn-details").text(myGame.players[myGame.turn].nameOfPlayer + ": Your turn total is: " + myGame.players[myGame.turn].turnTotal);
-    if(myGame.players[myGame.turn].rollSuccess === false) {
-      myGame.changeTurn();
-      $("#message").text(myGame.players[myGame.turn].nameOfPlayer + "s Turn");
-    }else if(myGame.win()){
-      $("#message").text(myGame.players[myGame.turn].nameOfPlayer +" rolled a " + myGame.players[myGame.turn].currentRoll + " and Wins!!!");
-      $("#start").fadeIn();
-      $("#in-progress").hide();
-      $("#turn-details").text("");
-      $("#roll-details").text("");
-      myGame.resetGame();
-    }
-  });
-  $("#hold").click(function(){
-    if(myGame.lastroll){
-      myGame.hold();
-      $("#message").text(myGame.players[myGame.turn].nameOfPlayer + "s Turn");
-    }
-    else $("#message").text("You cant hold! You rolled a one!");
-  });
+  $("#roll").click(roll);
+  $("#hold").click(hold);
 });
